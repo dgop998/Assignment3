3f62da2de51c9bddaff6e21f64d08fbdad93c601
Dharma <dgop998@aucklanduni.ac.nz>
2018-06-01 12:55:46 +1200

    Initial Commit
    First Commit

---
+++ HelloUnity_Final/Assets/Boat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Make sure you get rid of the namespace that is automatically generated here.
+class Boat
+{
+    //List all the columns of your table here in the same format as below.
+    public string BoatID { get; set; }
+    //public string Manufacturer { get; set; }
+    public string BoatName { get; set; }
+    //public string Segment { get; set; }
+}
